f2953add9fb70f50462fc96b8732693a678e159d
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-02-12 14:30:30 +0200

    Handle datasets with no snapshots at all

--- datasets_with_no_snapshots.sh
+++ datasets_with_no_snapshots.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-root="${1}"
+# Due to the way how zorg works, you *need* at least one snapshot to back up
+# the dataset. This helps you to find datasets which do not have any snapshots.
+
 scriptdir="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+root="${1}"
 zfs list -H -r -p -o name "${root}" | grep -v "^${root}$" | while read -r dataset; do
 	snapshots="$(zfs list -H -r -p -t snapshot -o name "${dataset}" 2>/dev/null | cut -d@ -f 2-)"
-	if ! grep -q -v -x -E -f "${scriptdir}/ignored.txt" <<< "${snapshots}"; then
+	if [ -z "${snapshots}" ] || ! grep -q -v -x -E -f "${scriptdir}/ignored.txt" <<< "${snapshots}"; then
 		echo "${dataset}"
 	fi
 done
