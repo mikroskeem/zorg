072440329091bfc3d3fe65954c94a24865b47284
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-03-05 13:51:18 +0200

    Allow disabling bindfs when writing data

--- mount_remap.sh
+++ mount_remap.sh
@@ -14,6 +14,7 @@ if [ "${UID}" -ne 0 ]; then
 fi
 
 : "${ZORG_REMAP_READONLY:=true}"
+: "${ZORG_REMAP_WRITE_NO_BINDFS:=false}"
 
 if [ -z "${_IN_NS:-}" ]; then
 	# shellcheck disable=SC2046
@@ -37,11 +38,13 @@ fi
 
 mnt_final=""
 mountflags=()
+is_dataset=1
 if grep -q "@" <<< "${dataset}"; then
 	IFS=@ read -r -a _snapshot <<< "${dataset}"
 	_dataset="${_snapshot[0]}"
 	_snap="${_snapshot[1]}"
 	mnt_final="/tmp/${_snap}"
+	is_dataset=0
 else
 	# work around zfs annoying mount mechanism
 	if ! [ "$(zfs get -H -o value mountpoint "${dataset}")" = "legacy" ]; then
@@ -54,18 +57,23 @@ else
 fi
 
 mount -t tmpfs tmpfs /tmp
-mnt_dataset="/tmp/.real/dataset"
-mkdir -p "${mnt_dataset}" "${mnt_final}"
+if (( is_dataset )) && ! [ "${ZORG_REMAP_READONLY}" = "true" ] && [ "${ZORG_REMAP_WRITE_NO_BINDFS}" = "true" ]; then
+	mkdir -p "${mnt_final}"
+	mount -t zfs -o "$(IFS=,; echo "${mountflags[*]}")" "${dataset}" "${mnt_final}"
+else
+	mnt_dataset="/tmp/.real/dataset"
+	mkdir -p "${mnt_dataset}" "${mnt_final}"
 
-mount -t zfs -o "$(IFS=,; echo "${mountflags[*]}")" "${dataset}" "${mnt_dataset}"
-bindfs -u "${_NS_UID}" -g "${_NS_GID}" "${mnt_dataset}" "${mnt_final}" -f &
-bpid="${!}"
+	mount -t zfs -o "$(IFS=,; echo "${mountflags[*]}")" "${dataset}" "${mnt_dataset}"
+	bindfs -u "${_NS_UID}" -g "${_NS_GID}" "${mnt_dataset}" "${mnt_final}" -f &
+	bpid="${!}"
 
-cleanup () {
-	umount "${mnt_final}" || kill -9 "${bpid}"
-	wait "${bpid}"
-}
-trap 'cleanup' EXIT
+	cleanup () {
+		umount "${mnt_final}" || kill -9 "${bpid}"
+		wait "${bpid}"
+	}
+	trap 'cleanup' EXIT
+fi
 
 #findmnt --poll=mount --first-only --timeout=$((10 * 1000)) --mountpoint "${mnt_final}" # TODO: crashes with double free???
 while [ -z "$(awk -v "mnt=${mnt_final}" '$2 == mnt { print $2 }' /proc/mounts)" ]; do
