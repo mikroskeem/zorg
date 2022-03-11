f016c5428b6f44d886a748e0c856b1fb880bbab2
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-03-12 00:53:40 +0200

    Use ZFS shim for macOS

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    openzfs-osx-shim-nix.url = "github:mikroskeem/openzfs-osx-shim-nix";
+
+    openzfs-osx-shim-nix.inputs.nixpkgs.follows = "nixpkgs";
+    openzfs-osx-shim-nix.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, openzfs-osx-shim-nix }:
     let
       supportedSystems = [
         "aarch64-linux"
@@ -17,47 +21,29 @@
     in
     flake-utils.lib.eachSystem supportedSystems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            openzfs-osx-shim-nix.overlay
+          ];
+        };
+
+        inherit (pkgs) lib;
       in
       rec {
         devShell = pkgs.mkShell {
-          buildInputs =
-            let
-              # Implies that https://openzfsonosx.org/ is installed on the system
-              zfs-mac' = { stdenvNoCC, zfsPath ? "/usr/local/zfs" }: stdenvNoCC.mkDerivation rec {
-                name = "mac-zfs-user";
-                phases = [ "installPhase" ];
-
-                installPhase = ''
-                  runHook preInstall
-
-                  mkdir -p $out
-                  for output in bin include lib libexec share; do
-                    ln -s "${zfsPath}/$output" $out/$output
-                  done
-
-                  runHook postInstall
-                '';
-              };
-              zfs-mac = pkgs.callPackage zfs-mac' { };
-            in
-            with pkgs; [
-              bindfs
-              borgbackup
-              gnupg
-              rage
-              shellcheck
-              sops
-            ] ++ lib.optionals stdenv.isDarwin [
-              zfs-mac
-              (sanoid.override {
-                zfs = zfs-mac;
-                mbuffer = null; # does not compile
-              })
-            ] ++ lib.optionals stdenv.isLinux [
-              sanoid
-            ];
+          buildInputs = with pkgs; [
+            bindfs
+            borgbackup
+            gnupg
+            rage
+            (sanoid.override (lib.optionalAttrs stdenv.isDarwin {
+              mbuffer = null; # does not compile
+            }))
+            shellcheck
+            sops
+            zfs
+          ];
         };
       });
 }
